fd24e83fd51243f7e71515504c729bf9933218d6
ifeegoo <root@ifeegoo.com>
2015-09-02 19:28:45 +0800

    First commit.

---
+++ smart-code-united-wifi-ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  smart-code-united-wifi-ios
+//
+//  Created by ifeegoo on 15/9/2.
+//  Copyright (c) 2015年 Smart Code United. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
